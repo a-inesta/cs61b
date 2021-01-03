4f49e01de9bb492e6326865a918465a53f847542
hengliao <hengliao1997@163.com>
2021-01-03 16:20:18 +0800

    After finishing add String to the test fail ouput, and i don't know what else should i do.

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+    StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
+    ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
+    @Test
+    public void testWhat() {
+
+        sad1.addFirst(3);
+        ads1.addFirst(3);
+        assertEquals(sad1.size(), ads1.size());
+        assertEquals(sad1.removeFirst(),ads1.removeFirst());
+        assertEquals(sad1.removeFirst(),ads1.removeFirst());
+        assertEquals(sad1.size(), ads1.size());
+
+        sad1.addLast(3);
+        ads1.addLast(3);
+        assertEquals(sad1.size(), ads1.size());
+        Integer x = 3;
+        assertEquals(sad1.removeFirst(), x);
+        assertEquals(ads1.removeFirst(), x);
+        assertEquals(sad1.size(), ads1.size());
+
+        sad1.addFirst(5);
+        sad1.addFirst(3);
+        Integer expected = 7;
+        Integer actual = sad1.removeFirst();
+        assertEquals("Oh noooo!\nThis is bad:\n   Random number " + actual
+                + " not equal to " + expected + "!", expected, actual);
+
+    }
+
+}
