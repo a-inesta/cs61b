0266998e8d8a847c7f6ae8d2b009279dfb8c9644
hengliao <hengliao1997@163.com>
2021-01-03 16:50:54 +0800

    fix some bugs complete proj1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -14,6 +14,7 @@ public class TestOffByOne {
         assertTrue(offByOne.equalChars('x', 'Y'));
         assertFalse(offByOne.equalChars('a', 'e'));
         assertFalse(offByOne.equalChars('a', 'a'));
+        assertTrue(offByOne.equalChars('&', '%'));
     }
     //Uncomment this class once you've created your CharacterComparator interface
     // and OffByOne class. *
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -5,15 +5,16 @@ public class TestPalindrome {
     // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
-    static CharacterComparator cc = new OffByOne();
+    static CharacterComparator obo = new OffByOne();
+    static CharacterComparator obn = new OffByN(2);
     @Test
     public void testWordToDeque() {
         Deque d = palindrome.wordToDeque("synergy");
-        String actual = "synergy";
+        String actual = "";
         for (int i = 0; i < "synergy".length(); i++) {
             actual += d.removeFirst();
         }
-        assertEquals("", actual);
+        assertEquals("synergy", actual);
     } //Uncomment this class once you've created your Palindrome class.
 
     @Test
@@ -26,9 +27,11 @@ public class TestPalindrome {
         assertFalse(palindrome.isPalindrome(input2));
         assertTrue(palindrome.isPalindrome(input3));
         assertTrue(palindrome.isPalindrome(input4));
-        assertTrue(palindrome.isPalindrome("abcb", cc));
-        assertFalse(palindrome.isPalindrome("abba", cc));
-        assertTrue(palindrome.isPalindrome("flake", cc));
+        assertTrue(palindrome.isPalindrome("abcb", obo));
+        assertFalse(palindrome.isPalindrome("abba", obo));
+        assertTrue(palindrome.isPalindrome("flake", obo));
+        assertTrue(palindrome.isPalindrome("A",obo));
+        assertTrue(palindrome.isPalindrome("Ab",obo));
     }
 
     /*@Test
